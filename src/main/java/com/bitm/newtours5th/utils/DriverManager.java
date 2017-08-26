5bd59e9008d6e79cb22a2ea3bb8ed1918ecf93e7
alifqa7 <alifqa7@gmail.com>
2017-08-26 17:57:35 +0600

    commit 3

---
+++ src/main/java/com/bitm/newtours5th/utils/DriverManager.java
@@ -0,0 +1,21 @@
+package com.bitm.newtours5th.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverManager {
+	
+	static {
+		System.setProperty("webdriver.gecko.driver", "E:/Selenium 5th batch/geckodriver-v0.18.0-win64/geckodriver.exe");
+	}
+	public static WebDriver driver= new FirefoxDriver ();
+	
+	public void driverexecution()
+	{
+		System.out.println("Driver executed!");
+	}
+	private DriverManager () {
+		
+	}
+
+}
